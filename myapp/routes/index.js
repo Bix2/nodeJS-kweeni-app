7351ed16294fd06ae539013a5d0448f3bf39c715
Bix2 <bibi.treffers@outlook.com>
2018-04-12 13:24:22 +0200

    Defined all routes

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Homepage' });
+});
+
+/* GET kweeni page. */
+router.get('/kweeni', function(req, res, next) {
+  res.render('kweeni', { title: 'Kweeni' });
+});
+
+/* GET details page. */
+router.get('/kweeni/:id', function(req, res, next) {
+  console.log('Request Id:', req.params.id);
 });
 
 module.exports = router;
