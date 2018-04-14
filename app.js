321f06390c85744665619abee55df82ad3d44e7f
Bix2 <bibi.treffers@outlook.com>
2018-04-14 18:31:16 +0200

    configuration of facebook-passport

--- app.js
+++ app.js
@@ -3,15 +3,48 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+var Users = require('./models/User');
+
+var app = express();
+
+// configure the Facebook strategy for use by Passport
+passport.use(new Strategy({
+  clientID: '226457874572897',
+  clientSecret: 'bec4b902be325e182532a48530533072',
+  callbackURL: 'https://8992a753.ngrok.io/login/facebook/return',
+  profileFields: ['id','displayName', 'photos'],
+}, 
+// function must invoke `cb` with a user object, which will be set at `req.user` in route handlers
+function(accessToken, refreshToken, profile, cb) {
+ 
+    Users.findOne({'id': profile.id}, function(err, user) {
+      if (err) 
+        return cb(err);
+      if (user) {
+        return cb(null, user);
+      } else {
+        var user = new Users();
+        user.id = profile.id;
+        user.name = profile.displayName;
+        user.avatar = profile.photos[0].value;
+        user.save(function(err) {
+          if (err)
+            throw err;
+          return cb(null, profile)
+        })
+      }
+    })
+}));
 
 // mongoose
 mongoose.connect('mongodb+srv://WebtechProject:kweeni123@clustermongo-jm8tv.mongodb.net/', {dbName: "kweeni"});
 var connection = mongoose.connection;
-
-
 connection.on('error', console.error.bind(console, 'connection error:'));
 connection.once('open', function(){
   console.log('Successfully connected');
@@ -21,9 +54,20 @@ connection.once('open', function(){
   })
 });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var app = express();
+// configure Passport authenticated session persistence
+// serialize users into and deserialize out of the session to restore authentication state across HTTP requests
+
+// supply the user ID when serializing
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+// querying the user record by ID when deserializing
+passport.deserializeUser(function(id, cb) {
+  Users.findOne({'id': profile.id}, function(err, user) {
+    cb(err, user);
+  });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +80,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+
+// initialize Passport and restore authentication state
+app.use(passport.initialize());
+app.use(passport.session());
 
 // routes
 app.use('/', indexRouter);
