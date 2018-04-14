dab900647c86154c85f635aee9a1d87301c6442a
Bix2 <bibi.treffers@outlook.com>
2018-04-14 14:25:19 +0200

    set urlencoded extended to true

--- app.js
+++ app.js
@@ -23,20 +23,21 @@ connection.once('open', function(){
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// setup express app
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.json());
+
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
