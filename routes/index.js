e50e75f687c778d42f2f02f7ed10927f12fad939
Bix2 <bibi.treffers@outlook.com>
2018-04-14 18:23:43 +0200

    routes for facebook authentication and logging out

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,14 +7,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Homepage' });
 });
 
+// redirect user to facebook for authentication
+router.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+// facebook will redirect the user to this URL after approval
+router.get('/login/facebook/return',
+  passport.authenticate('facebook', { failureRedirect: '/'}),
+  function(req, res) {
+    // redirect user to /kweeni route
+    res.redirect('/kweeni');
+  })
+
 /* GET kweeni page. */
 router.get('/kweeni', function(req, res, next) {
-  res.render('kweeni', { title: 'Kweeni' });
+  res.render('kweeni', { 
+    title: 'Kweeni',
+    user: req.user 
+  });
 });
 
 /* GET details page. */
 router.get('/kweeni/:id', function(req, res, next) {
   console.log('Request Id:', req.params.id);
+});
+
+// route for logging out
+router.get('/users/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
