152787243cb7602543fb648553adf7c35bcb5323
Bix2 <bibi.treffers@outlook.com>
2018-04-15 16:48:06 +0200

    added type to _id for reference in other models

--- models/User.js
+++ models/User.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    _id: Schema.Types.ObjectId,
     id: String,
     name: String,
     avatar: String
 })
 
-const Users = mongoose.model('Users', userSchema);
+const User = mongoose.model('Users', userSchema);
 
-module.exports = Users;
+module.exports = User;
