60fbb18872da654817386565bd95ddd64c558967
Bix2 <bibi.treffers@outlook.com>
2018-04-27 16:58:23 +0200

    deleted cookieParser

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: true }));
